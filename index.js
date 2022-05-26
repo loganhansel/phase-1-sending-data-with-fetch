12be151d8973db0f90436cbcd9b0a20ed1b67d66
loganhansel <loganhansel23@gmail.com>
2022-05-26 17:03:21 -0400

    commit tree d186a37b52c338d77818bab8473206a8b98be8d7

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+
+function submitData(userName, userEmail){
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({name: userName, email: userEmail})
+    })
+    .then(function (response) {
+        return response.json()})
+    .then(function (object) {
+        document.body.append(object.id)
+    })
+    .catch(function (error) {
+        alert("Error!");
+        document.body.append(error.message);
+    })
+}
+
